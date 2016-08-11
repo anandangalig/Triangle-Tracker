e5b8f5120630796d57c99a88d7e0fb4222c14cd3
Anand Angalig and Rebecca Zarsky <rebecca@>
2016-08-11 16:17:42 -0400

    fix the show/hide bug of results

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,27 @@ $(document).ready(function(){
     var sideOneInput = parseInt($("input#side1").val());
     var sideTwoInput = parseInt($("input#side2").val());
     var sideThreeInput = parseInt($("input#side3").val());
-    var scaleneRule =
 
-    if ((sideOneInput + sideTwoInput) < sideThreeInput || (sideOneInput + sideThreeInput) < sideTwoInput || (sideTwoInput + sideThreeInput) < sideOneInput) {
-      $(".not").show();
+    if ((sideOneInput + sideTwoInput) < sideThreeInput  || (sideOneInput + sideThreeInput) < sideTwoInput || (sideTwoInput + sideThreeInput) < sideOneInput) {
+      $("#not").show();
+      $("#equilateral").hide();
+      $("#scalene").hide();
+      $("#isosceles").hide();
     } else if (sideOneInput === sideTwoInput && sideOneInput === sideThreeInput) {
       $("#equilateral").show();
+      $("#scalene").hide();
+      $("#not").hide();
+      $("#isosceles").hide();
     } else if (sideOneInput === sideTwoInput || sideOneInput === sideThreeInput || sideTwoInput === sideThreeInput) {
       $("#isosceles").show();
+      $("#equilateral").hide();
+      $("#scalene").hide();
+      $("#not").hide();
     } else {
       $("#scalene").show();
+      $("#equilateral").hide();
+      $("#not").hide();
+      $("#isosceles").hide();
     }
   });
 });

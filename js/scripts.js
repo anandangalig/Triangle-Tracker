cfc03715bee8a63099fcce07aa0465c36fc792ed
Anand Angalig and Rebecca Zarsky <anand+rebecca@>
2016-08-11 15:57:24 -0400

    add final functionality to triangle triangulator

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,16 @@ $(document).ready(function(){
     var sideOneInput = parseInt($("input#side1").val());
     var sideTwoInput = parseInt($("input#side2").val());
     var sideThreeInput = parseInt($("input#side3").val());
+    var scaleneRule =
 
-    if (sideOneInput === sideTwoInput && sideOneInput === sideThreeInput) {
+    if ((sideOneInput + sideTwoInput) < sideThreeInput || (sideOneInput + sideThreeInput) < sideTwoInput || (sideTwoInput + sideThreeInput) < sideOneInput) {
+      $(".not").show();
+    } else if (sideOneInput === sideTwoInput && sideOneInput === sideThreeInput) {
       $("#equilateral").show();
+    } else if (sideOneInput === sideTwoInput || sideOneInput === sideThreeInput || sideTwoInput === sideThreeInput) {
+      $("#isosceles").show();
+    } else {
+      $("#scalene").show();
     }
   });
 });
